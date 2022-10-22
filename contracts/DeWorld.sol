4b99284fd38dd5639c3435571e8ddb9c313c1f83
Harnick Khera <20884880+Hephyrius@users.noreply.github.com>
2022-10-22 16:04:37 +0100

    finish the basic world creation

--- contracts/DeWorld.sol
+++ contracts/DeWorld.sol
@@ -1,11 +1,15 @@
 pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./DeFarm.sol";
 import "hardhat/console.sol";
 
 contract DeWorld is Ownable {
 
     mapping(uint => mapping(uint => address)) world;
+    mapping(address => address) farmOwners;
+    address[] allFarms;
+
     uint xSize = 1000;
     uint ySize = 1000;
 
@@ -20,16 +24,17 @@ contract DeWorld is Ownable {
     function createPlot(uint x, uint y) public {
         require(x < xSize && y < ySize, "Out of Bounds");
         require(world[x][y] != address(0), "A Farm Already Exists");
-        _deployFarm();
+        require(farmOwners[msg.sender] == address(0), "Already Own A Farm");
+        address plot = _deployFarm();
     }
 
     /*
     *   internal function used to deploy a farm at (x,y)
     */
-    function _deployFarm() internal {
-
+    function _deployFarm() internal returns (address plot){
+        plot = new DeFarm(msg.sender);
+        farmOwners[msg.sender] = plot;
+        allFarms.push(plot);
     }
-
-
 
 }
